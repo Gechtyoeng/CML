8286508e368f99444e80e5fbe2b92c86273b5cb4
OengGechty <gechtyoeng@gmail.com>
2025-02-22 15:06:17 +0700

    add appoinment with basic method

--- src/javaCode/Appointment.java
+++ src/javaCode/Appointment.java
@@ -1,5 +1,61 @@
 package javaCode;
 
-public class Appointment {
+import java.time.LocalDateTime;
 
+public class Appointment {
+    private static int idcounter = 1;   //auto increment id
+    private int appointmentId;
+    private Patient patient;      // Corresponds to patient class
+    private Doctor doctor;       // Corresponds to Docotr class
+    private LocalDateTime appointmentDate;
+    private String status;
+
+    public Appointment(Patient patient, Doctor doctor, LocalDateTime appointmentDate, String status) {
+        this.appointmentId = idcounter++;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.appointmentDate = appointmentDate;
+        this.status = "Scheduled";
+    }
+
+    //getters
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+    public Patient getPatient() {
+        return patient;
+    }
+    public Doctor getDoctor() {
+        return doctor;
+    }
+    public LocalDateTime getAppointmentDate() {
+        return appointmentDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    //setter
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    //mark appointment as completed
+    public void markAsCompleted() {
+        this.status = "Completed";
+    }
+    //mark appointment as cancelled
+    public void markAsCancelled() {
+        this.status = "Cancelled";
+    }
+    //mark appointment as rescheduled
+    public void markAsRescheduled() {
+        this.status = "Rescheduled";
+    }
+    //display appointment details
+    public void displayAppointmentDetails() {
+        System.out.println("Appointment ID: " + appointmentId);
+        System.out.println("Patient: " + patient.getFirstName() + " " + patient.getLastName());
+        System.out.println("Doctor: " + doctor.getFirstName() + " " + doctor.getLastName());
+        System.out.println("Appointment Date: " + appointmentDate);
+        System.out.println("Status: " + status);
+    }
 }
