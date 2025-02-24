77ed85cbf3cfe8f43f2edebd9c541ea8a405541f
OengGechty <gechtyoeng@gmail.com>
2025-02-24 12:53:32 +0700

    add doctor and patients with method for booking system

--- src/javaCode/Doctor.java
+++ src/javaCode/Doctor.java
@@ -1,5 +1,51 @@
 package javaCode;
 
-public class Doctor {
+import java.util.List;
+import java.util.ArrayList;
+import util.base.Person;
 
+public class Doctor extends Person{
+    private String specialization;
+    private List<Appointment> appointments;
+
+    public Doctor(String username, String password, String firstName, String lastName, String email, String phone, String specialization) {
+        super(username, password, firstName, lastName, email, phone);
+        this.specialization = specialization;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getSpecialization() {
+        return specialization;
+    }
+    public void displayDoctorDetails() {
+        System.out.println("Doctor ID: " + id);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Specialization: " + specialization);
+    }
+   public List<Appointment> getAppointments() {
+        return appointments;
+    }
+    public void addAppointment(Appointment appointment) {
+        if(appointments == null) {
+            appointments = new ArrayList<>();
+        }
+        appointments.add(appointment);
+    }
+    public void removeAppointment(Appointment appointment) {
+        if(appointments.contains(appointment)) {
+            appointments.remove(appointment);
+            System.out.println("Appointment removed successfully");
+        }else {
+            System.out.println("Appointment not found");
+        }
+    }
+    
 }
--- src/javaCode/Patient.java
+++ src/javaCode/Patient.java
@@ -1,5 +1,41 @@
 package javaCode;
 
-public class Patient {
+import java.util.List;
+import java.util.ArrayList;
+import util.base.Person;
 
+public class Patient extends Person {
+
+    private String gender;
+    private String dateOfBirth;
+    private String address;
+    private List<Appointment> appointments; //store all appointments of patient
+
+    public Patient(String username, String password, String firstName, String lastName, String email, String phone, String 
+    gender, String dateOfBirth, String address) {
+        super(username, password, firstName, lastName, email, phone);
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void addAppointment(Appointment appointment) {
+        if(appointments == null) {
+            appointments = new ArrayList<>();
+        }
+        appointments.add(appointment);
+    }
+    public void showAppointments() {
+        for (Appointment appointment : appointments) {
+            appointment.displayAppointmentDetails();
+        }
+    }
 }
