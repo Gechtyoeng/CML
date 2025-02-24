020bb52b9c9b38104534de4cde01489e184d5324
SSONita <sarsovannita4@gmail.com>
2025-02-25 01:10:06 +0700

    add method to inventory and medicine

--- src/javaCode/Inventory.java
+++ src/javaCode/Inventory.java
@@ -1,5 +1,174 @@
 package javaCode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Inventory {
 
+    private int inventoryId;     // Corresponds to inventory_id
+    private String itemName;     // Corresponds to item_name
+    private int quantity;        // Corresponds to quantity
+    private double price;        // Corresponds to price
+    private LocalDate expiryDate;   // Corresponds to expiry_date(yyyy-mm-dd)
+
+     // Constructor
+    public Inventory(int inventoryId, String itemName, int quantity, double price, LocalDate expiryDate) {
+        this.inventoryId = inventoryId;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+        this.expiryDate = expiryDate;
+    }
+
+    private static List<Inventory> inventoryList = new ArrayList<>();
+
+     // Getters and Setters
+     public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    //method
+
+    //Add new items
+    public void addItems(Inventory item){
+        inventoryList.add(item);
+        System.out.println("Item " + item.getItemName() + " added sucessfully!");
+
+    }
+
+    //Update items by ID(Modify quantity, price, or expiry date)
+    public void updateItems(int inventoryId, int newQuantity, double newPrice, LocalDate newExpiryDate){
+        for(Inventory item : inventoryList){
+            if(item.getInventoryId() == inventoryId){
+                item.setQuantity(newQuantity);
+                item.setPrice(newPrice);
+                item.setExpiryDate(newExpiryDate);
+                System.out.println("Item updated sucessfully!");
+                return;
+            }
+        }
+        System.out.println("Item with ID " + inventoryId + " not found.");
+    }
+
+    //Remove items by ID
+    public void removeItems(int inventoryId){
+        Iterator <Inventory> iterator = inventoryList.iterator();
+
+        while (iterator.hasNext()) {
+            Inventory item = iterator.next();
+            if(item.inventoryId == inventoryId){
+                iterator.remove();
+                System.out.println("Item " + item.getItemName() + " removed sucessfully!");
+                return;
+            }
+        }
+        System.out.println("Item with ID " + inventoryId + " not found.");
+    }
+
+    //Search for an item by name or ID
+    public void searchItemsByID(int inventoryId){
+        for(Inventory item : inventoryList){
+            if(item.getInventoryId() == inventoryId){
+                System.out.println("Item found: " + item.getItemName());
+                return;
+            }
+        }
+        System.out.println("Item with ID " + inventoryId + " not found.");
+    }
+
+    public void searchItemsByName(String itemName){
+        for(Inventory item :inventoryList){
+            if(item.getItemName().equalsIgnoreCase(itemName)){
+                System.out.println("Item found: " + item.getItemName());
+                return;
+            }
+        }
+        System.out.println("Item with ID " + inventoryId + " not found.");
+    }
+
+    //Display all inventory items
+    public void displayInventory(){
+        if (inventoryList.isEmpty()) {
+            System.out.println("Inventory is empty.");
+            return;
+        }
+        for (Inventory item : inventoryList) {
+            System.out.println("ID: " + item.getInventoryId() + ", Name: " + item.getItemName() +
+                    ", Quantity: " + item.getQuantity() + ", Price: " + item.getPrice() +
+                    ", Expiry Date: " + item.getExpiryDate());
+        }
+    }
+
+    //checking the expiration date of the items
+    public void expiryCheck(LocalDate expiryDate){
+        LocalDate currTime = LocalDate.now();
+        System.out.println("Expiry Status of all items:");
+
+        for(Inventory item : inventoryList){
+        
+            if(currTime.isBefore(item.getExpiryDate())){
+                System.out.println("The item " + item.getItemName() + " is not expired yet!");
+            }else if(currTime.isEqual(item.getExpiryDate())){
+                System.out.println("The item " + item.getItemName() + " is expired today!");
+            }else{
+                System.out.println("The item " + item.getItemName() + " have already expired!");
+            }
+        }
+    }
+
+    //checking quantity in stock by ID
+    public void quantityCheck(int inventoryId, String itemName, int quantity){
+         System.out.println("Stock left for " + inventoryId + " (" + itemName + "): " + quantity);
+    }
+
+    //checking stock level
+    public void stockLevel(){
+        int lowStock = 5;
+
+        for(Inventory item : inventoryList){
+            if(item.getQuantity() <= lowStock){
+                System.out.println(item.getItemName() + ": Low stock (only " + item.getQuantity() + " left)");
+            }else{
+                System.out.println(item.getItemName() + ": Sufficient Stock (" + item.getQuantity() + " left)");
+            }
+        }
+    }
 }
--- src/javaCode/Medicine.java
+++ src/javaCode/Medicine.java
@@ -1,5 +1,166 @@
 package javaCode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Medicine {
+    private int medicineId;
+    private String medicineName;
+    private String dosage;
+    private double price;
+    private int stock;
+    private LocalDate expiryDate;
+    private String description;
+
+    // Constructor
+    public Medicine(int medicineId, String medicineName, String dosage, double price, int stock, LocalDate expiryDate, String description) {
+        this.medicineId = medicineId;
+        this.medicineName = medicineName;
+        this.dosage = dosage;
+        this.price = price;
+        this.stock = stock;
+        this.expiryDate = expiryDate;
+        this.description = description;
+    }
+
+    private static List<Medicine> medList = new ArrayList<>();
+
+    // getter
+    public int getMedicineID(){
+        return medicineId;
+    }
+    public String getMedicineName(){
+        return medicineName;
+    }
+    public String getDosage(){
+        return dosage;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getStock(){
+        return stock;
+    }
+    public LocalDate getExpiryDate(){
+        return expiryDate;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    //setter
+    public void setMedicineId(int medicineId) {
+        this.medicineId = medicineId;
+    }
+    public void setName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //method
+    public boolean isExpired() {
+        return LocalDate.now().isAfter(expiryDate);
+    }
+
+    public void veiwMedicinInfoByID(int medicineId){
+        for(Medicine med : medList){
+            if(med.getMedicineID() == medicineId){
+                med.veiwMedicinInfo();
+                return;
+            }
+        }
+        System.out.println("Medicine with ID " + medicineId + " not found.");
+    }
+
+    public void veiwMedicinInfoByName(String medicineName){
+        for(Medicine med : medList){
+            if(med.getMedicineName().equalsIgnoreCase(medicineName)){
+                med.veiwMedicinInfo();
+                return;
+            }
+        }
+        System.out.println("Medicine with ID " + medicineId + " not found.");
+    }
+
+    public void veiwMedicinInfo(){
+        System.out.println("Medicine ID: " + medicineId);
+        System.out.println("Name: " + medicineName);
+        System.out.println("Dosage: " + dosage);
+        System.out.println("Price: $" + price);
+        System.out.println("Stock: " + stock);
+        System.out.println("Expiry Date: " + expiryDate);
+        System.out.println("Expired: " + (isExpired() ? "Yes" : "No"));
+        System.out.println("Description: " + description);
+    }
+
+    public void addMedicine(Medicine med){
+        medList.add(med);
+        System.out.println("Medicine " + med.getMedicineName() + " added sucessfully!");
+    }
+
+    public void removeMedicine(int medicineId){
+        Iterator <Medicine> iterator = medList.iterator();
+
+        while (iterator.hasNext()) {
+            Medicine med = iterator.next();
+            if(med.getMedicineID() == medicineId){
+                iterator.remove();
+                System.out.println("Medicine ID " + med.getMedicineID() + " removed sucessfully!");
+                return;
+            }
+        }
+        System.out.println("Medicine with ID " + medicineId + " not found.");
+    }
+
+    public void updateMedicine(int medicineId, double newPrice, int newStock, LocalDate newExpiryDate, String newDescription){
+        for(Medicine med : medList){
+            if(med.getMedicineID() == medicineId){
+                med.setPrice(newPrice);
+                med.setStock(newStock);
+                med.setExpiryDate(newExpiryDate);
+                med.setDescription(newDescription);
+                return;
+            }
+        }
+        System.out.println("Medicine with ID " + medicineId + " not found.");
+    }
+
+    public void displayAllTheMedicine(){
+        if(medList.isEmpty()){
+            System.out.println("Database is empty!");
+            return;
+        }
+        for(Medicine med : medList){
+            med.veiwMedicinInfo();
+        }
+    }
+
+    public void medStockLevel(){
+        int medLowStock = 5;
+
+        for(Medicine med : medList){
+            if(med.getStock() <= medLowStock){
+                System.out.println(med.getMedicineName() + ": Low stock (only " + med.getStock() + " left)");
+            }else{
+                System.out.println(med.getMedicineName() + ": Sufficient Stock (" + med.getStock() + " left)");
+            }
+        }
+    }
 
 }
